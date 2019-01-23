5f31e165ecd2f9112b74e9385ebd1af868ad4466
shivgoel <shivgoel@adobe.com>
2019-01-24 00:30:14 +0530

    Converting get call to post

--- src/main/java/com/prj/api/fileApp/Controller/FileController.java
+++ src/main/java/com/prj/api/fileApp/Controller/FileController.java
@@ -8,9 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prj.api.fileApp.Exception.FileProcessingException;
@@ -28,8 +28,8 @@ public class FileController {
 		return "Server running";
 	}
 
-	@RequestMapping(value = "/commonWords", method = RequestMethod.GET)
-	public ResponseEntity<List<String>> getCommonWordsFromFiles(@RequestParam("path") String path)
+	@RequestMapping(value = "/commonWords", method = RequestMethod.POST)
+	public ResponseEntity<List<String>> getCommonWordsFromFiles(@RequestBody String path)
 			throws FileProcessingException {
 		try {
 			List<String> commonWords = new ArrayList<>();
